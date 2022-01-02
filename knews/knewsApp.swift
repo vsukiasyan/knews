cc4e9377797fdff19fd668eda669a3bd9f4eb843
vsukiasyan <vsukiasyan1@hotmail.com>
2022-01-02 09:29:18 -0800

    Initial Commit

---
+++ knews/knewsApp.swift
@@ -0,0 +1,20 @@
+//
+//  knewsApp.swift
+//  knews
+//
+//  Created by Vic Sukiasyan on 1/2/22.
+//
+
+import SwiftUI
+
+@main
+struct knewsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
